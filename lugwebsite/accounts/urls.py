8ec40a84b351222cbb635551415198a0c38ee676
Aditya Kumar Singh <adityakumar2809@gmail.com>
2019-01-18 23:29:17 +0530

    Make static folders

--- lugwebsite/accounts/urls.py
+++ lugwebsite/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
--- lugwebsite/accounts/views.py
+++ lugwebsite/accounts/views.py
@@ -9,3 +9,4 @@ class SignUp(CreateView):
 	form_class = forms.UserCreateForm
 	success_url = reverse_lazy('login')
 	template_name = 'accounts/signup.html'
+
