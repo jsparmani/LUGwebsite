434756023d1b63b012f04a62dec177634becce10
Aditya Kumar Singh <adityakumar2809@gmail.com>
2019-01-18 16:12:56 +0530

    Add login and signup for any general user

---
+++ lugwebsite/accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.db import models
+from django.contrib import auth
+from accounts.models import User
+class UserCreateForm(UserCreationForm):
+
+	class Meta():
+		fields = ('first_name','last_name','username','email','password1','password2')
+		model = User
+
+		def __init__(self,*args,**kwargs):
+			super.__init__(*args,**kwargs)
+			self.fields['username'].label='Username'
+			self.fields['first_name'].label='First Name'
+			self.fields['last_name'].label='Last Name'
+			self.fields['email'].label='Email Address'
---
+++ lugwebsite/lugwebsite/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+	template_name = 'index.html'
+
+class WelcomePage(TemplateView):
+	template_name = 'welcome.html'
+
+class ThanksPage(TemplateView):
+	template_name = 'thanks.html'
